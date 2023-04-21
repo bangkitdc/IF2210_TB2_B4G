b0b9e11786ee280c6aefb3d4e7811e55acf702ab
bangkitdc <bangkitdc@gmail.com>
2023-04-22 00:20:26 +0700

    setup: gradle and javafx

---
+++ app/src/main/java/bnmobusinessmanagementsystem/App.java
@@ -0,0 +1,23 @@
+package bnmobusinessmanagementsystem;
+
+/* Import library */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(new StackPane(label), 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
