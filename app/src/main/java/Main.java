1eb73434b61b167dfba5ff4390bf29c8d8df5408
Muhammad Bangkit Dwi Cahyono <87227379+bangkitdc@users.noreply.github.com>
2023-04-26 15:04:59 +0700

    Delete Main.java

--- app/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import bnmobusinessmanagementsystem.models.customer.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Customer a=new Customer();
-        System.out.println(a.getCustomerId());
-    }
-}
