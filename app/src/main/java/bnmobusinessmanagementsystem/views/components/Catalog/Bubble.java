0c533fbed96e561ce2219bbab7f667a5d1ccaeab
irsyadnb <irsyadxd@gmail.com>
2023-05-06 12:58:36 +0700

    feat : inventaris

---
+++ app/src/main/java/bnmobusinessmanagementsystem/views/components/Catalog/Bubble.java
@@ -0,0 +1,128 @@
+package bnmobusinessmanagementsystem.views.components.Catalog;
+
+import bnmobusinessmanagementsystem.models.Item;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class Bubble extends BorderPane {
+    private Label bubblePict;
+    private Label bubbleName;
+    private Label bubblePrice;
+    private Button removeItem;
+    private VBox rightSide;
+    private Label quantityLabel;
+    private int quantity;
+    public Bubble(Item item){
+        quantity = 1;
+        bubblePict = new Label(item.getImage());
+        bubbleName = new Label(item.getName());
+        bubblePrice = new Label(Double.toString(item.getSellPrice()));
+        removeItem = new Button("X");
+        removeItem.setAlignment(Pos.CENTER);
+        removeItem.setTextAlignment(TextAlignment.CENTER);
+
+        bubblePict.setPrefHeight(50);
+        bubblePict.setPrefWidth(50);
+        bubblePict.setBackground(Background.fill(Color.BLACK));
+
+        bubblePrice.setTextAlignment(TextAlignment.RIGHT);
+
+        Button incrementButton = new Button("+");
+        incrementButton.setOnAction(event -> {
+            removeItem = new Button("X");
+            quantity++;
+            double res = item.getSellPrice()*quantity;
+            rightSide.getChildren().setAll(new Label("Rp"+res), removeItem);
+            this.setRight(rightSide);
+            quantityLabel.setText(Integer.toString(quantity));
+            bubblePrice.setText(Double.toString(res));
+        });
+
+        Button decrementButton = new Button("-");
+        decrementButton.setOnAction(event -> {
+            if (quantity > 1) {
+                quantity--;
+                double res = item.getSellPrice()*quantity;
+                rightSide.getChildren().setAll(new Label("Rp"+res), removeItem);
+                this.setRight(rightSide);
+                quantityLabel.setText(Integer.toString(quantity));
+                bubblePrice.setText(Double.toString(res));
+            }
+        });
+
+        // Create the quantity label
+        quantityLabel = new Label(Integer.toString(quantity));
+
+        // Create the HBox containing the buttons and the label
+        HBox quantityBox = new HBox();
+        quantityBox.setSpacing(10);
+        quantityBox.getChildren().addAll(decrementButton, quantityLabel, incrementButton);
+
+        rightSide = new VBox();
+        rightSide.setSpacing(20);
+        rightSide.getChildren().addAll(bubblePrice, removeItem);
+        this.setStyle("-fx-background-color: #f0f0f0;");
+        this.setLeft(bubblePict);
+        this.setRight(rightSide);
+        this.setCenter(bubbleName);
+        this.setBottom(quantityBox);
+    }
+
+    public void setBubbleName(Label bubbleName) {
+        this.bubbleName = bubbleName;
+    }
+
+    public Label getBubbleName() {
+        return bubbleName;
+    }
+
+    public void setBubblePict(Label bubblePict) {
+        this.bubblePict = bubblePict;
+    }
+
+    public Label getBubblePict() {
+        return bubblePict;
+    }
+
+    public void setBubblePrice(Label bubblePrice) {
+        this.bubblePrice = bubblePrice;
+    }
+
+    public Label getBubblePrice() {
+        return bubblePrice;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantityLabel(Label quantityLabel) {
+        this.quantityLabel = quantityLabel;
+    }
+
+    public Label getQuantityLabel() {
+        return quantityLabel;
+    }
+
+    public Button getRemoveItem() {
+        return removeItem;
+    }
+
+    public void setRemoveItem(Button removeItem) {
+        this.removeItem = removeItem;
+    }
+}
