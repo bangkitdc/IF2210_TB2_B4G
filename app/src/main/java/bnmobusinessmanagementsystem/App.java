44da98b8fefb09d4a0bd4c11b8abbb9bb6d63d99
irsyadnb <irsyadxd@gmail.com>
2023-05-06 21:20:39 +0700

    feat : datastore

--- app/src/main/java/bnmobusinessmanagementsystem/App.java
+++ app/src/main/java/bnmobusinessmanagementsystem/App.java
@@ -1,27 +1,19 @@
 package bnmobusinessmanagementsystem;
 
 /* Import library */
-import bnmobusinessmanagementsystem.views.Inventaris;
 import bnmobusinessmanagementsystem.views.MenuAndTab;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.*;
 
 public class App extends Application {
 
-    private Button newCustomer;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
-//            MenuAndTab menuBar = new MenuAndTab(new Scene(new VBox(), 1080, 660));
-//            primaryStage.setScene(menuBar.getScene());
-//            primaryStage.show();
-            Inventaris inventaris = new Inventaris ();
-            primaryStage.setScene(inventaris.getScene());
-//            primaryStage.setmaximized(true
+            MenuAndTab menuBar = new MenuAndTab(new Scene(new VBox(), 1080, 660));
+            primaryStage.setScene(menuBar.getScene());
             primaryStage.show();
             primaryStage.setResizable(false);
 
