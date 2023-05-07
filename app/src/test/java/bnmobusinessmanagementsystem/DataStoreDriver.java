89c04283aa296167875165c57255e24a8b667247
zaydanA <16521131@std.stei.itb.ac.id>
2023-05-07 15:01:44 +0700

    push

--- app/src/test/java/bnmobusinessmanagementsystem/DataStoreDriver.java
+++ app/src/test/java/bnmobusinessmanagementsystem/DataStoreDriver.java
@@ -2,6 +2,7 @@ package bnmobusinessmanagementsystem.utils;
 
 //import DataStore;
 import bnmobusinessmanagementsystem.models.customer.*;
+import bnmobusinessmanagementsystem.models.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,9 +14,20 @@ public class DataStoreDriver {
         DataStore dataStore = new DataStore("tes.json");
 //
         Customer[] customers = new Customer[2];
-        customers[0] = new VIP("Jane Doe", "987654321");
-        customers[1] = new Member("Joli Diva", "123456789");
+        customers[0] = new VIP("Jane Doe", "987654321", "id1");
+        customers[1] = new Member("Joli Diva", "123456789", "id2");
 //        dataStore.addCustomer((customers));
+        Item[] items = new Item[2];
+        items[0] = new Item("KONTOL", 1.0, 2.0, 100, "kontol", "kontoool");
+        items[1] = new Item("MEMEK", 1.0, 2.0, 100, "memek", "kontoool");
+        ArrayList<Item> itemL = new ArrayList<>(Arrays.asList(items));
+
+        Purchase[] purchases = new Purchase[1];
+        purchases[0] = new Purchase("id1", "asdfghjkl", itemL, 69.69);
+        ArrayList<Purchase> purchaseL = new ArrayList<>(Arrays.asList(purchases));
+
+        customers[0].setTransaction(purchaseL);
+
         ArrayList<Customer> customerList = new ArrayList<>(Arrays.asList(customers));
 
         dataStore.saveCustomer(customerList);
